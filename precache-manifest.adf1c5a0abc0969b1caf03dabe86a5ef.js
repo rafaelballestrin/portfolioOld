5720fa27736ebb85aabda0735d872ab8dd85a685
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-01-31 10:19:21 +0000

    Updates

---
+++ precache-manifest.adf1c5a0abc0969b1caf03dabe86a5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "25a9f1ec7ca81c5ec568",
+    "url": "/react-gh-pages/static/js/main.25a9f1ec.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "25a9f1ec7ca81c5ec568",
+    "url": "/react-gh-pages/static/css/main.38b25211.chunk.css"
+  },
+  {
+    "revision": "53f07355384ed0e04b5579821f62101d",
+    "url": "/react-gh-pages/index.html"
+  }
+];
