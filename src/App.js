af7059550609a47d0ccb51041b931a2476598e52
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-02-04 21:34:28 +0000

    CSS changes and created HomeContent

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Gallery from './Gallery';
 import Details from './Details';
+import HomeContent from './HomeContent.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="Page">
           <div className="App">
             <Route exact path='/' component={Gallery} />
+            <Route exact path='/' component={HomeContent} />
             <Route exact path='/:galleryId' component={Details} />
           </div>
         </div>
--- src/Gallery.js
+++ src/Gallery.js
@@ -7,9 +7,6 @@ import contact from './images/contact.svg';
 export default function Gallery() {
   return (
     <div>
-      <div>
-        <h1>Hello, my name is Rafael. I'm a Front End Developer living in London, UK.</h1>
-      </div>
       <div className='container'>
         <Stack id='aboutme' name='About me' logo={about} />
         <Stack id='projects' name='Projects' logo={projects} />
