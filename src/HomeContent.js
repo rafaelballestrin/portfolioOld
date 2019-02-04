547f6771888fa7669c7140a6cba70d79cb7bb68d
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-02-04 21:54:30 +0000

    Update HomeContent with Text and CSS on home content, padding.

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function HomeContent() {
     return (
-        <div>HomeContent</div>
+        <div className="HomeContent">
+            <h3>Hi! I'm Rafael</h3>
+            <h3>I'm a Front-end Developer living in London</h3>
+        </div>
     )
 }
