c80a6d839bae9d352877f1c7d12241cb37a4d9a2
Rafael Ballestrin <rafaelballestrin@gmail.com>
2019-02-07 15:14:39 +0000

    Css Changes

--- src/HomeContent/HomeContent.js
+++ src/HomeContent/HomeContent.js
@@ -4,11 +4,17 @@ import ComputerImg from '../images/computerDev.svg';
 
 export default function HomeContent() {
     return (
-        <div className="HomeContent">
+        <div className='HomeContent'>
             <Menu />
-            <h3>Home Content</h3>
-            <h4>I'm a Front-end Developer living in London</h4>
-            <img src= {ComputerImg} alt=''/>
+            <div className='wrap'>
+                <div className='left'>
+                    <h3>Home Content</h3>
+                    <h4>I'm a Front-end Developer living in London</h4>
+                </div>
+                <div className='right'>
+                    <img src={ComputerImg} alt='' />
+                </div>
+            </div>
         </div>
     )
 }
